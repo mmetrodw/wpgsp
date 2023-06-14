8e99cc0a4b766d7e26460e3b06c68c01993ded78
mmetrodw <artgure@gmail.com>
2023-06-14 20:47:48 +0000

    modified:   assets/js/wpgsp-script.js modified:   index.html

--- assets/js/wpgsp-script.js
+++ assets/js/wpgsp-script.js
@@ -1,1 +1,18 @@
-console.log('test');
+const seek = document.querySelector('.wpgsp-audio-seek');
+const seekWidth = seek.clientWidth;
+const count = Math.floor(seekWidth / 10);
+alert(count)
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  
+
+for(i = 0; i < count; i++){
+    let html_to_insert = '<div class="wpgsp-bar" style="height: ' + getRandomInt(0, 60) + 'px"></div>';
+    seek.insertAdjacentHTML('beforeend', html_to_insert);
+}
